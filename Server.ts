d9d36376595e62834114ef128319a5a540607d99
sofia <sofia@Inspirion-W10>
2018-05-10 12:10:09 +0200

    mit kommentaren

--- Server.ts
+++ Server.ts
@@ -7,8 +7,8 @@ namespace Server {
     }
 
     let port: number = process.env.PORT;
-    if (port == undefined)
-        port = 8100;
+    if (port == undefined) // wenn Port undefined dann
+        port = 8100;        // setze ihn auf port 8100
 
     let server: Http.Server = Http.createServer();
     server.addListener("listening", handleListen);
@@ -22,12 +22,12 @@ namespace Server {
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("Ich hÃ¶re Stimmen!");
 
-        let query: AssocStringString = Url.parse(_request.url, true).query;
+        let query: AssocStringString = Url.parse(_request.url, true).query; // Url.parse() Metohde -> wenn false dann Ergebnis "NaN"
         let a: number = parseInt(query["a"]);
         let b: number = parseInt(query["b"]);
 
-        for (let key in query) 
-            console.log(query[key]);
+        for (let key in query) // for-in-Schleife ist für Objekte gedacht, werden alle nicht belegten Array-Stellen einfach übergangen, iteriert über die Schlüssel des assoziativen Arrays
+            console.log(query[key]); // gibt query aus zu der Schlüssel 'key' passt
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
