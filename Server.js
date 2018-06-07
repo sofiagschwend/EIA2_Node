578a63e51979bed56bd2c70fd53e0de94990b355
Sofia <Sofia@DESKTOP-MS98UH4.eduroam.hs-furtwangen.de>
2018-06-07 11:40:05 +0200

    aktuell handlers mit SetHeaders

--- Server.js
+++ Server.js
@@ -9,10 +9,11 @@ var Server;
     if (port == undefined)
         port = 8100;
     let server = Http.createServer();
-    server.addListener("listening", handleListen);
+    //server.addListener("listening", handleListen);
+    server.addListener("request", handleRequestSetHeaders);
     server.addListener("request", handleRequest);
     server.listen(port);
-    function handleListen(_request, _response) {
+    function handleRequestSetHeaders(_request, _response) {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
     }
